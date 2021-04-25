038bb33c96e6eca6cef735421d836939427a8f24
angelagongli <angelagongli@gmail.com>
2021-04-25 17:58:43 -0500

    Add Header.js

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <h1 className="header">
+        <i className="ms-Icon ms-Icon--Back" aria-hidden="true"></i>
+        &nbsp;The Six-Foot Office&nbsp;
+        <i className="ms-Icon ms-Icon--Forward" aria-hidden="true"></i>
+    </h1>
+  );
+}
+
+export default Header;
