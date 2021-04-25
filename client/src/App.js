79a04a46d7769e0b1ea76e3ffe8e89877009e25e
angelagongli <angelagongli@gmail.com>
2021-04-25 17:55:56 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import OfficeSchedule from "./components/OfficeSchedule";
-import OfficeFloorPlan from "./components/OfficeFloorPlan";
+import Home from "./pages/Home";
+import TeamPage from "./pages/TeamPage";
+import EmployeePage from "./pages/EmployeePage";
 import '@fluentui/react/dist/css/fabric.css';
+import "./assets/css/style.css";
 
 function App() {
   return (
     <div>
-      <OfficeSchedule />
-      <OfficeFloorPlan />
+      <Router>
+        <Switch>
+          <Route exact path="/team">
+            <TeamPage />
+          </Route>
+          <Route exact path="/employee">
+            <EmployeePage />
+          </Route>
+          <Route>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
