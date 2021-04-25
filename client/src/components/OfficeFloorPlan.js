e3e79b7427a0d20788f81768dc7cb2970cb5618c
angelagongli <angelagongli@gmail.com>
2021-04-25 17:56:42 -0500

    Update OfficeFloorPlan.js

--- client/src/components/OfficeFloorPlan.js
+++ client/src/components/OfficeFloorPlan.js
@@ -5,11 +5,11 @@ function OfficeFloorPlan(props) {
         <div>
             <map name="OfficeFloorPlanMap">
                 <area shape="rect" coords="440, 55, 620, 190"
-                    href="https://github.com/angelagongli/TheSixFootOffice"
-                    target="_blank" alt="SeatNumber1" />
+                    href="/#/seat/1" onClick={() => props.chooseSeat(1)}
+                    target="_self" alt="SeatNumber1" />
                 <area shape="rect" coords="620, 55, 800, 190"
-                    href="https://github.com/angelagongli/TheSixFootOffice#readme"
-                    target="_blank" alt="SeatNumber2" />
+                    href="/#/seat/2" onClick={() => props.chooseSeat(2)}
+                    target="_self" alt="SeatNumber2" />
                 <area shape="rect" coords="800, 55, 970, 190"
                     href="https://github.com/angelagongli/TheSixFootOffice/blob/main/README.md"
                     target="_blank" alt="SeatNumber3" />
