5d25f2bc65675f05c037d39e88e61c0a8ca937dd
angelagongli <angelagongli@gmail.com>
2021-04-25 17:59:13 -0500

    Add Navigation.js

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Nav } from '@fluentui/react/lib/Nav';
+
+const navStyles = {
+  root: {
+    width: "100%",
+    height: "100%",
+    boxSizing: 'border-box',
+    border: '1px solid #eee',
+    overflowY: 'auto',
+  },
+};
+
+const navLinkGroups = [
+  {
+    links: [
+      {
+        name: 'Home',
+        url: '/',
+        key: 'Home',
+        target: '_self',
+      },
+      {
+        name: 'Team',
+        url: '/team',
+        key: 'Team',
+        target: '_self',
+      },
+      {
+        name: 'Employee',
+        url: '/employee',
+        key: 'Employee',
+        target: '_self',
+      },
+      {
+        name: 'Make Event',
+        url: '/event',
+        key: 'Event',
+        target: '_self',
+      }
+    ],
+  },
+];
+
+export const Navigation = () => {
+  return (
+    <Nav
+      ariaLabel="Navigation"
+      styles={navStyles}
+      groups={navLinkGroups}
+    />
+  );
+};
