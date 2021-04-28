4909aba1ef73acb36110dc5ab48d7984f6594366
angelagongli <angelagongli@gmail.com>
2021-04-28 18:13:22 -0500

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
 const employeeRoutes = require("./employees");
 const teamRoutes = require("./teams");
+const employeeScheduleRoutes = require("./employeeSchedules");
+const teamScheduleRoutes = require("./teamSchedules");
+const dayRoutes = require("./days");
+const eventRoutes = require("./events");
 
 router.use("/employees", employeeRoutes);
 router.use("/teams", teamRoutes);
+router.use("/employeeSchedules", employeeScheduleRoutes);
+router.use("/teamSchedules", teamScheduleRoutes);
+router.use("/days", dayRoutes);
+router.use("/events", eventRoutes);
 
 module.exports = router;
