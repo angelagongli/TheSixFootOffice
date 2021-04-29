df16ccefeec2df62429f526527dbac3c29ca0b24
angelagongli <angelagongli@gmail.com>
2021-04-28 20:59:41 -0500

    Update Seat.js

--- client/src/components/Seat.js
+++ client/src/components/Seat.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Persona, PersonaInitialsColor, PersonaSize } from '@fluentui/react/lib/Persona';
+import API from "../utils/API";
+import EmployeeSchedule from "./EmployeeSchedule";
 
 function Seat(props) {
+    const [employeeSchedule, setEmployeeSchedule] = useState();
+    const today = new Date();
+    const beginningOfWeek = new Date(today.setDate(today.getDate() - today.getDay()));
+
+    useEffect(() => {
+        loadEmployeeSchedule();
+    }, [props.employee]);
+
+    function loadEmployeeSchedule() {
+        API.getEmployeeScheduleByEmployeeWeek(props.employee.id,
+            `${beginningOfWeek.getFullYear()}-${beginningOfWeek.getMonth() + 1}-${beginningOfWeek.getDate() + 1}`)
+            .then(res => {
+                setEmployeeSchedule(res.data);
+                console.log("Employee's schedule set");
+            }).catch(err => console.log(err));
+    }
+
     return (
         <div>
-            Seat Number: {props.seatNumber}
+            <h5>
+                Seat Number: {props.employee.seatNumber}
+            </h5>
             <Persona text={props.employee.name}
                 secondaryText={`Employee ID: ${props.employee.id},
-                    Employee's Team: ${props.team.name}`}
+                    Employee's Team: ${props.employee.Team.name}`}
                 showSecondaryText={true}
                 size={PersonaSize.size32}
                 initialsColor={PersonaInitialsColor.cyan} />
+            {employeeSchedule && employeeSchedule.Days.length ?
+            <EmployeeSchedule days={employeeSchedule.Days} />
+            : ""}
         </div>
     );
 }
