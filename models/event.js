86c4916a87b82a1b6c9f2f75a60ca9e66def5807
angelagongli <angelagongli@gmail.com>
2021-05-05 18:11:38 -0500

    Update Event model in event.js

--- models/event.js
+++ models/event.js
@@ -2,16 +2,25 @@ module.exports = function(sequelize, DataTypes) {
     const today = new Date();
 
     const Event = sequelize.define("Event", {
-        name: DataTypes.STRING,
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: DataTypes.STRING,
         date: {
             type: DataTypes.DATEONLY,
             allowNull: false,
             defaultValue: today
         },
-        time: {
-            type: DataTypes.DATE,
+        startTime: {
+            type: DataTypes.TIME,
             allowNull: false,
-            defaultValue: sequelize.fn('NOW')
+            defaultValue: today
+        },
+        endTime: {
+            type: DataTypes.TIME,
+            allowNull: false,
+            defaultValue: today
         },
         isRecurring: {
             type: DataTypes.BOOLEAN,
