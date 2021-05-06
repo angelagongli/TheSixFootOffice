a4bdd93ab85bd9e0397a51b4bb9bdefe0ccb1088
angelagongli <angelagongli@gmail.com>
2021-05-05 22:45:21 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TeamPage from "./pages/TeamPage";
 import EmployeePage from "./pages/EmployeePage";
+import EventPage from "./pages/EventPage";
 import '@fluentui/react/dist/css/fabric.css';
 import "./assets/css/style.css";
 
@@ -16,6 +17,9 @@ function App() {
           </Route>
           <Route exact path="/employee">
             <EmployeePage />
+          </Route>
+          <Route exact path="/event">
+            <EventPage />
           </Route>
           <Route>
             <Home />
