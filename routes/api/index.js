43dcd9da9e7c73b7ea72e98b9c1c5f8a5188940a
angelagongli <angelagongli@gmail.com>
2021-05-09 09:31:14 -0500

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,6 +5,7 @@ const employeeScheduleRoutes = require("./employeeSchedules");
 const teamScheduleRoutes = require("./teamSchedules");
 const dayRoutes = require("./days");
 const eventRoutes = require("./events");
+const recurringEventRoutes = require("./recurringEvents");
 
 router.use("/employees", employeeRoutes);
 router.use("/teams", teamRoutes);
@@ -12,5 +13,6 @@ router.use("/employeeSchedules", employeeScheduleRoutes);
 router.use("/teamSchedules", teamScheduleRoutes);
 router.use("/days", dayRoutes);
 router.use("/events", eventRoutes);
+router.use("/recurringEvents", recurringEventRoutes);
 
 module.exports = router;
