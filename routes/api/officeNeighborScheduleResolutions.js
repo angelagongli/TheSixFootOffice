376fa320fe2ea8086c500de5b8d842bd413ba2cb
angelagongli <angelagongli@gmail.com>
2021-05-16 11:29:48 -0500

    Add officeNeighborScheduleResolutions.js

---
+++ routes/api/officeNeighborScheduleResolutions.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const officeNeighborScheduleResolutionController = require("../../controllers/officeNeighborScheduleResolutionController");
+
+router.route("/employee/:id")
+    .get(officeNeighborScheduleResolutionController.findAllByEmployee);
+
+router.route("/employee/:id/:week")
+    .get(officeNeighborScheduleResolutionController.findByEmployeeWeek);
+
+router.route("/week/:week")
+    .get(officeNeighborScheduleResolutionController.findAllByWeek);
+
+router.route("/")
+    .get(officeNeighborScheduleResolutionController.findAll);
+
+router.route("/:id")
+    .put(officeNeighborScheduleResolutionController.update);
+
+module.exports = router;
