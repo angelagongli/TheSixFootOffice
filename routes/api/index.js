2fe8533f3a0811c5d78d1439cd8e540f566c8789
angelagongli <angelagongli@gmail.com>
2021-05-16 12:32:59 -0500

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,6 +7,10 @@ const dayRoutes = require("./days");
 const eventRoutes = require("./events");
 const recurringEventRoutes = require("./recurringEvents");
 const officeSettingRoutes = require("./officeSettings");
+const officeNeighborScheduleResolutionRoutes = require("./officeNeighborScheduleResolutions");
+const officeNeighborScheduleRequestRoutes = require("./officeNeighborScheduleRequests");
+const officeNeighborScheduleRequestDayRoutes = require("./officeNeighborScheduleRequestDays");
+const officeNeighborScheduleResultRoutes = require("./officeNeighborScheduleResults");
 
 router.use("/employees", employeeRoutes);
 router.use("/teams", teamRoutes);
@@ -16,5 +20,9 @@ router.use("/days", dayRoutes);
 router.use("/events", eventRoutes);
 router.use("/recurringEvents", recurringEventRoutes);
 router.use("/officeSettings", officeSettingRoutes);
+router.use("/officeNeighborScheduleResolutions", officeNeighborScheduleResolutionRoutes);
+router.use("/officeNeighborScheduleRequests", officeNeighborScheduleRequestRoutes);
+router.use("/officeNeighborScheduleRequestDays", officeNeighborScheduleRequestDayRoutes);
+router.use("/officeNeighborScheduleResults", officeNeighborScheduleResultRoutes);
 
 module.exports = router;
