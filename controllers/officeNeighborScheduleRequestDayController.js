7e2afc721634ebdb681dbf1a194636de3c66b46b
angelagongli <angelagongli@gmail.com>
2021-05-16 12:31:08 -0500

    Add officeNeighborScheduleRequestDayController.js

---
+++ controllers/officeNeighborScheduleRequestDayController.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+
+module.exports = {
+    findAll: function(req, res) {
+        db.OfficeNeighborScheduleRequestDay.findAll({
+            include: [
+                {
+                    model: db.OfficeNeighborScheduleRequest
+                }
+            ]
+        }).then(dbOfficeNeighborScheduleRequestDaysAll => {
+            res.json(dbOfficeNeighborScheduleRequestDaysAll);
+        });
+    },
+    findAllByOfficeNeighborScheduleRequest: function(req, res) {
+        db.OfficeNeighborScheduleRequestDay.findAll({
+            include: [
+                {
+                    model: db.OfficeNeighborScheduleRequest
+                }
+            ],
+            where: { OfficeNeighborScheduleRequestId: req.params.id }
+        }).then(dbOfficeNeighborScheduleRequestDaysOfOfficeNeighborScheduleRequestAll => {
+            res.json(dbOfficeNeighborScheduleRequestDaysOfOfficeNeighborScheduleRequestAll);
+        });
+    },
+    update: function(req, res) {
+        db.OfficeNeighborScheduleRequestDay.update(req.body, {
+            where: { id: req.params.id }
+        }).then(dbOfficeNeighborScheduleRequestDay => {
+            res.json(dbOfficeNeighborScheduleRequestDay);
+        }).catch(err => {
+            res.status(422).json(err);
+        });
+    }
+}
