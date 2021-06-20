89e1caaec690c0586ac3fab27d054eab462924ca
angelagongli <angelagongli@gmail.com>
2021-06-20 08:30:14 -0500

    Update officeNeighborScheduleRequestController.js

--- controllers/officeNeighborScheduleRequestController.js
+++ controllers/officeNeighborScheduleRequestController.js
@@ -10,7 +10,8 @@ module.exports = {
                 {
                     model: db.OfficeNeighborScheduleRequestDay
                 }
-            ]
+            ],
+            order: [['createdAt', 'DESC'], ['nearestOfficeNeighborRole']]
         }).then(dbOfficeNeighborScheduleRequestsAll => {
             res.json(dbOfficeNeighborScheduleRequestsAll);
         });
@@ -25,7 +26,8 @@ module.exports = {
                     model: db.OfficeNeighborScheduleRequestDay
                 }
             ],
-            where: { OfficeNeighborScheduleResolutionId: req.params.id }
+            where: { OfficeNeighborScheduleResolutionId: req.params.id },
+            order: [['createdAt', 'DESC'], ['nearestOfficeNeighborRole']]
         }).then(dbOfficeNeighborScheduleRequestsOfOfficeNeighborScheduleResolutionAll => {
             res.json(dbOfficeNeighborScheduleRequestsOfOfficeNeighborScheduleResolutionAll);
         });
@@ -34,6 +36,14 @@ module.exports = {
         db.OfficeNeighborScheduleRequest.update(req.body, {
             where: { id: req.params.id }
         }).then(dbOfficeNeighborScheduleRequest => {
+            res.json(dbOfficeNeighborScheduleRequest);
+        }).catch(err => {
+            res.status(422).json(err);
+        });
+    },
+    create: function(req, res) {
+        db.OfficeNeighborScheduleRequest.create(req.body)
+        .then(dbOfficeNeighborScheduleRequest => {
             res.json(dbOfficeNeighborScheduleRequest);
         }).catch(err => {
             res.status(422).json(err);
